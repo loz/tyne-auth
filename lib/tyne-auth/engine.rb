beed997b23a3ca95ec67b5274d9ed0226c0edb72
Michael Leßnau <michael.lessnau@googlemail.com>
2012-06-06 06:55:18 +0200

    setup rspec

--- lib/tyne-auth/engine.rb
+++ lib/tyne-auth/engine.rb
@@ -1,5 +1,10 @@
 module TyneAuth
   class Engine < ::Rails::Engine
     isolate_namespace TyneAuth
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
