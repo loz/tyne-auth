28f0b1e78a532172a5f01af175e8252d011a75cb
Michael Leßnau <michael.lessnau@googlemail.com>
2012-06-08 23:49:53 +0200

    made devise views available for overwriting

--- app/models/tyne_auth/user.rb
+++ app/models/tyne_auth/user.rb
@@ -1,12 +1,8 @@
 module TyneAuth
   class User < ActiveRecord::Base
-    # Include default devise modules. Others available are:
-    # :token_authenticatable, :confirmable,
-    # :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
 
-    # Setup accessible (or protected) attributes for your model
     attr_accessible :firstname, :lastname, :email, :password, :password_confirmation, :remember_me
   end
 end
